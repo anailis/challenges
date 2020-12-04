8c53d5fa13db2528cef0e9955ccf2f8564c03a30
FUMMEY Eilidh <s1533194@sms.ed.ac.uk>
2020-12-04 22:07:56 +0000

    complete density chart

--- TidyTuesday/tidytuesdaywk48.R
+++ TidyTuesday/tidytuesdaywk48.R
@@ -1,8 +1,11 @@
 library(pacman)
 
+setwd('C:/Users/s1533194/Documents/Learning/Challenges/TidyTuesday')
+
 p_load(tidytuesdayR, tidyverse, magrittr,
        tidytext, quanteda, fastDummies,
-       GGally)
+       GGally, jpeg, grid, waffle, extrafont,
+       hrbrthemes, ggpubr, png)
 tuesdata <- tidytuesdayR::tt_load(2020, week = 48)
 hike_data <- tuesdata$hike_data
 
@@ -40,11 +43,6 @@ sum(hike_pairs$rating==0)
 location_means <- hike_data_dummy %>%
   group_by(location) %>%
   summarise_if(is.numeric, mean)
-ggpairs(location_means)
-
-hike_data_dummy %>%
-  group_by(location) %>%
-  summarise_if(is.factor, sum)
 
 # extract key words from descriptions
 descriptions <- corpus(hike_data_dummy$description, 
@@ -58,6 +56,17 @@ token_matrix <- dfm(clean_tokens) %>%
   dfm_trim(min_termfreq = 10) 
 cotokens <- fcm(clean_tokens)
 
+# theme: what makes a trail good for kids?
+## word cloud
+## density graph
+## graphic
+## waffle chart
+
+loadfonts(device = "win")
+
+perc <- c(sum(hike_data_dummy$features_Good_for_kids), nrow(hike_data_dummy) - sum(hike_data_dummy$features_Good_for_kids))
+waffle(c(6,10), use_glyph = "child")
+
 # top 30 words
 tokens_250 <- names(topfeatures(token_matrix, 250))
 tm_250 <- token_matrix %>%
@@ -67,4 +76,65 @@ tm_250 <- token_matrix %>%
 kids_matrix <- dfm(clean_tokens, groups = "features_Good_for_kids") %>%
   dfm_select(min_nchar = 2) %>% 
   dfm_trim(min_termfreq = 10) 
-textplot_wordcloud(loc_matrix, comparison = TRUE, max_words = 50)
+
+#png("week48_wrdcld.png")
+wrdcld <- textplot_wordcloud(kids_matrix, comparison = TRUE, max_words = 50, color = c("#9CC1F8", "#3C8F7D"))
+#dev.off()
+
+# 
+density <- ggplot(aes(x = highpoint, fill = as.factor(features_Good_for_kids)),
+       data = hike_data_dummy) +
+  geom_density(alpha = 0.8, color = "white") + 
+  xlim(c(0,11000)) +
+  theme_bw() + 
+  theme(axis.text.y=element_blank(),
+        axis.text.x = element_text(size = 15, color = "#355389", vjust = 8),
+        axis.title = element_text(size = 20, colour = "#13375B"),
+        axis.title.y = element_text(angle = 0, vjust = 0.5),
+        axis.title.x = element_text(vjust = 5),
+        legend.position = c(0.8,0.8),
+        legend.background = element_rect(color = "white"),
+        legend.text = element_text(color = "#363636", size = 20, colour = "#13375B"),
+        legend.title = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid = element_blank()) +
+  xlab("Highest Point (feet)") + 
+  ylab("Number\nof Trails") +
+  scale_fill_manual(values = c("#9CC1F8", "#3C8F7D"),
+                    lab = c("Unsuitable for Children", "Suitable for Children"))
+
+ggsave('week48_density.png', density, units = "cm", width = 40, height = 20)
+
+
+ggplot(aes(x = as.factor(features_Summits), fill = as.factor(features_Good_for_kids)),
+       data = hike_data_dummy) +
+  geom_bar() +
+  theme_bw() + 
+  theme(axis.text.y=element_blank(),
+        axis.title.x = element_text(color = "#13375B"),
+        legend.position = c(0.8,0.8),
+        legend.background = element_rect(color = "white"),
+        legend.text = element_text(color = "#363636", color = "#13375B"),
+        legend.title = element_text(color = "#363636", color = "#13375B"),
+        panel.border = element_blank(),
+        axis.ticks = element_blank(),
+        panel.background = element_rect(fill = "#212752"),
+        panel.grid = element_line(color = "#4d5380")) + 
+  scale_fill_manual(values = c("#bababa", "#575757"), name = "Trail Suitable\nfor Children?", 
+                    lab = c("NO", "YES"))
+
+img <- readJPEG("images/8355.jpg")
+gimg <- rasterGrob(img, interpolate=TRUE)
+
+imgplt <- ggplot() +
+  annotation_custom(gimg, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
+
+wrdcld <- readPNG("wordcloud.png")
+gwrdcld <- rasterGrob(wrdcld, interpolate=TRUE)
+
+wrdcldplt <- ggplot() +
+  annotation_custom(gwrdcld, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)
+
+
+ggarrange(density, imgplt, wrdcldplt, nrow = 2, ncol = 2)
