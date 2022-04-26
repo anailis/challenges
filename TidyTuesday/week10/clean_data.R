e7e87488b0fbc29a6723ea1ff6317f492d7f0688
FUMMEY Eilidh <s1533194@ed.ac.uk>
2022-04-26 18:28:10 +0100

    clean erasmus data for Shiny app

---
+++ TidyTuesday/week10/clean_data.R
@@ -0,0 +1,91 @@
+library(tidyverse)
+library(tidytuesdayR)
+library(rnaturalearth)
+library(rnaturalearthdata)
+
+tuesdata <- tidytuesdayR::tt_load('2022-03-08')
+erasmus <- tuesdata$erasmus
+
+head(erasmus)
+dim(erasmus)
+names(erasmus)
+
+# I noticed that there are a lot of trips that are don't actually involve 
+# moving to a different country
+erasmus <- erasmus %>%
+  mutate(no_move = sending_country_code == receiving_country_code)
+# these can also have long durations, I really don't know what they represent
+# I couldn't find an explanation online
+# for now I just remove them
+ggplot(data = erasmus) +
+  geom_histogram(aes(x = mobility_duration, fill = no_move),
+                 bins = 50) + 
+  theme_bw()
+erasmus <- erasmus %>%
+  filter(!no_move)
+dim(erasmus)
+
+country_codes <- unique(c(erasmus$sending_country_code, 
+                          erasmus$receiving_country_code))
+# 54 unique country codes
+length(country_codes)
+
+world <- ne_countries(scale = "medium", 
+                       returnclass = "sf")
+
+# 51 of the 54 codes in the Erasmus dataset are identified 
+# in the world dataset
+rep_country_codes <- world %>%
+  filter(iso_a2 %in% country_codes) %>%
+  pull(iso_a2)
+
+# countries not identified: 
+## the UK (should be GB)
+## Greece (should be GR)
+## Kosovo (is not an ISO standard country, so has NA in world)
+setdiff(country_codes, rep_country_codes)
+
+# fix UK and Greece in erasmus dataframe
+erasmus <- erasmus %>%
+  mutate(across(contains("country_code"), ~ str_replace(., "UK", "GB"))) %>%
+  mutate(across(contains("country_code"), ~ str_replace(., "EL", "GR"))) 
+
+# fix Kosovo in the world dataframe
+world <- world %>%
+  mutate(iso_a2 = ifelse(name == "Kosovo", "XK", iso_a2)) %>%
+  mutate(erasmus = (iso_a2 %in% c(country_codes, "GB", "GR")))
+sum(world$erasmus)
+
+sent <- erasmus %>%
+  # use .drop = FALSE and levels = country_codes to include combinations with no counts 
+  # e.g. UK and Russia
+  mutate(across(contains("country_code"), ~ factor(., levels = country_codes))) %>%
+  group_by(sending_country_code, receiving_country_code, .drop = FALSE) %>%
+  summarise(total_participants = sum(participants), .groups = "keep") %>%
+  full_join(world %>% select(geometry, iso_a2), by = c("sending_country_code" = "iso_a2")) %>%
+  full_join(world %>% select(geometry, iso_a2), by = c("receiving_country_code" = "iso_a2"),
+            suffix = c(".sending", ".receiving"))
+
+# the basic plot when not being interacted with
+ggplot() +
+  geom_sf(aes(fill = erasmus), data = world) +
+  scale_fill_manual(values = c("#c2bfb8", "#d4a22f")) +
+  theme_bw() +
+  theme(
+    panel.border = element_blank(),
+    legend.position = "none"
+    )
+
+# what a plot would look like if user had clicked on UK
+ggplot() +
+  geom_sf(data = world) +
+  geom_sf(aes(geometry = geometry.receiving, fill = total_participants),
+          data = sent %>% filter(sending_country_code == "GB")) +
+  geom_sf(aes(geometry = geometry.sending), fill = "yellow",
+          data = sent %>% filter(sending_country_code == "GB")) +
+  theme_bw() +
+  theme(
+    panel.border = element_blank()
+  )
+
+write_csv(sent, "erasmus_exchanges.csv")
